44d2ab0775be5d6254445e444ea8e7dc0098e502
RukoleevNikita <rukoleev.nikita@yandex.ru>
2021-10-27 20:32:22 +0600

    test

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
     // $data = json_decode(file_get_contents('php://input'), TRUE);
     // file_put_contents('file.txt', '$data: '.print_r($data, 1)."\n", FILE_APPEND);
 
-    // echo("$data");
-    // echo("hi");
+    // // echo("$data");
+    // // echo("hi");
 
 
     // //https://api.telegram.org/bot*2020484979:AAHSsZuXd_rVRbWUC174VJbbW0aa2PtMxkU/setwebhook?url=*URL*
@@ -26,16 +26,36 @@ if (empty($data['message']['chat']['id'])) {
 	exit();
 }
  
-define('TOKEN', '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11');
-
-file_put_contents(__DIR__ . '/message.txt', print_r($data, true));
+define('TOKEN', '2020484979:AAHSsZuXd_rVRbWUC174VJbbW0aa2PtMxkU');
+ 
+// Функция вызова методов API.
+function sendTelegram($method, $response)
+{
+	$ch = curl_init('https://api.telegram.org/bot' . TOKEN . '/' . $method);  
+	curl_setopt($ch, CURLOPT_POST, 1);  
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $response);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HEADER, false);
+	$res = curl_exec($ch);
+	curl_close($ch);
+ 
+	return $res;
+}
 
 if (!empty($data['message']['text'])) {
 	$text = $data['message']['text'];
-	echo $text;
-}
+ 
+	if (mb_stripos($text, 'привет') !== false) {
+		sendTelegram(
+			'sendMessage', 
+			array(
+				'chat_id' => $data['message']['chat']['id'],
+				'text' => 'Хай!'
+			)
+		);
+ 
+		exit();	
+	} 
 
-    echo("$data");
-    echo("21");
 
    
