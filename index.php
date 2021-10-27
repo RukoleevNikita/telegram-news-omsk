b76c1365d1fdfb1e0c45116c188d373cf638aefe
RukoleevNikita <rukoleev.nikita@yandex.ru>
2021-10-27 20:15:28 +0600

    test

--- index.php
+++ index.php
@@ -1,22 +1,34 @@
 <?php
-    $data = json_decode(file_get_contents('php://input'), TRUE);
-    file_put_contents('file.txt', '$data: '.print_r($data, 1)."\n", FILE_APPEND);
+    // $data = json_decode(file_get_contents('php://input'), TRUE);
+    // file_put_contents('file.txt', '$data: '.print_r($data, 1)."\n", FILE_APPEND);
+
+    // echo("$data");
+    // echo("hi");
+
+
+    // //https://api.telegram.org/bot*2020484979:AAHSsZuXd_rVRbWUC174VJbbW0aa2PtMxkU/setwebhook?url=*URL*
+
+    // $token = '2020484979:AAHSsZuXd_rVRbWUC174VJbbW0aa2PtMxkU';
+
+    // $message = $data['message']['text'];
+
+    // $params = [
+    //     'chat_id' => $data['message']['chat']['id'],
+    //     'text'    => $message
+    // ];
+
+    // file_get_contents('https://api.telegram.org/bot'.$token.'/sendMessage?'.http_build_query($params));
+
+    $data = file_get_contents('php://input');
+$data = json_decode($data, true);
+ 
+if (empty($data['message']['chat']['id'])) {
+	exit();
+}
+ 
+define('TOKEN', '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11');
 
     echo("$data");
-    echo("hi");
-
-
-    //https://api.telegram.org/bot*2020484979:AAHSsZuXd_rVRbWUC174VJbbW0aa2PtMxkU/setwebhook?url=*URL*
-
-    $token = '2020484979:AAHSsZuXd_rVRbWUC174VJbbW0aa2PtMxkU';
-
-    $message = $data['message']['text'];
-
-    $params = [
-        'chat_id' => $data['message']['chat']['id'],
-        'text'    => $message
-    ];
-
-    file_get_contents('https://api.telegram.org/bot'.$token.'/sendMessage?'.http_build_query($params));
+    echo("21");
 
    
